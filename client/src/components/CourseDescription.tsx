0f5d1a401e7069a65b48ae4f6230117c08ba5c8f
Javed Nissar <contact@javednissar.ca>
2024-02-29 21:12:35 -0500

    Add CourseDescription with Chakra

--- client/src/components/CourseDescription.tsx
+++ client/src/components/CourseDescription.tsx
@@ -1,17 +1,28 @@
+import { Box, HStack, VStack, Text } from '@chakra-ui/react';
+import { IoCheckmark } from 'react-icons/io5';
+
+
 export const CourseDescription = ({ description, skills }: { description: string, skills: string }) => {
-    return (
-      <div className="course-description">
-        <h2>About this course</h2>
-        <p>{description}</p>
-        <div className="course-skills">
-          <h3>Skills you'll gain</h3>
-          <ul>
-            {skills.split(',').map(skill => (
-              <li key={skill}>{skill}</li>
+  return (
+    <Box p={5} m={5}>
+      <HStack spacing={8} align="start">
+        <VStack align="start" flex={8}>
+          <Text fontSize="2xl" fontWeight="bold" color="white">About this course</Text>
+          <Text color="white">{description}</Text>
+        </VStack>
+        <VStack align="start" flex={2}>
+          <Text fontSize="2xl" fontWeight="bold" color="white">Skills you'll gain</Text>
+          <VStack align="start" spacing={2}>
+            {skills.split(',').map((skill, index) => (
+              <HStack key={index} spacing={0}>
+                <IoCheckmark color="white" />
+                <Text as="li" color="white" sx={{ listStyleType: 'none', marginLeft: '5px' }}>{skill}</Text>
+              </HStack>
             ))}
-          </ul>
-        </div>
-      </div>
-    );
-  };
-  
+          </VStack>
+        </VStack>
+      </HStack>
+    </Box>
+  );
+};
+
