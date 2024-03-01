4dc20020524ba46bc778d7d9ee01227c07150af7
Javed Nissar <contact@javednissar.ca>
2024-02-29 20:52:41 -0500

    Complete CourseMeta refactor with Chakra

--- client/src/components/CourseMeta.tsx
+++ client/src/components/CourseMeta.tsx
@@ -1,41 +1,50 @@
-import { FaUserGraduate, FaClock, FaCertificate, FaCheckSquare } from 'react-icons/fa';
-import { Card, CardHeader, CardBody, Text, Heading, SimpleGrid, HStack } from '@chakra-ui/react'
+import { FiBookOpen } from 'react-icons/fi';
+import { BsClockHistory } from 'react-icons/bs';
+import { PiCertificate, PiListMagnifyingGlassThin } from 'react-icons/pi';
+
+import { Box, Text, HStack, VStack } from '@chakra-ui/react'
 
 export const CourseMeta = ({ level, timeToComplete, prerequisites }: { level: string, timeToComplete: string, prerequisites: string }) => {
   return (
-    <HStack>
-      <Card>
-        <CardHeader>
-          <Heading size='md'> Skill Level </Heading>
-        </CardHeader>
-        <CardBody>
-          <Text> {level} </Text>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardHeader>
-          <Heading size='md'> Time to complete </Heading>
-        </CardHeader>
-        <CardBody>
-          <Text> {timeToComplete} </Text>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardHeader>
-          <Heading size='md'> Certificate </Heading>
-        </CardHeader>
-        <CardBody>
-          <Text> At the end of the course </Text>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardHeader>
-          <Heading size='md'> Prerequisites </Heading>
-        </CardHeader>
-        <CardBody>
-          <Text> {prerequisites} </Text>
-        </CardBody>
-      </Card>
-    </HStack>
+    <Box p={5} mx={5} border="1px solid" borderColor="gray.700" borderRadius="lg" >
+      <HStack w="full" justify="center" spacing={10}>
+        <HStack spacing={3}>
+          <FiBookOpen size="25px" color="gray" />
+          <VStack spacing={0} align="start" >
+            <Text fontSize="x-small" color="gray">
+              Skill level
+            </Text>
+            <Text color="white" fontWeight="bold">{level}</Text>
+          </VStack>
+        </HStack>
+        <HStack spacing={3}>
+          <BsClockHistory size="25px" color="gray"></BsClockHistory>
+          <VStack spacing={0} align="start" >
+            <Text fontSize="x-small" color="gray">
+              Time to complete
+            </Text>
+            <Text color="white" fontWeight="bold">{timeToComplete}</Text>
+          </VStack>
+        </HStack>
+        <HStack spacing={3}>
+          <PiCertificate color="gray" size="25px"></PiCertificate>
+          <VStack spacing={0} align="start" >
+            <Text fontSize="x-small" color="gray">
+              Certificate
+            </Text>
+            <Text color="white" fontWeight="bold">At the end of the course</Text>
+          </VStack>
+        </HStack>
+        <HStack spacing={3}>
+          <PiListMagnifyingGlassThin color="gray" size="25px"></PiListMagnifyingGlassThin>
+          <VStack spacing={0} align="start">
+            <Text fontSize="x-small" color="gray">
+              Prerequisites
+            </Text>
+            <Text color="white" fontWeight="bold">{prerequisites}</Text>
+          </VStack>
+        </HStack>
+      </HStack>
+    </Box>
   )
 };
