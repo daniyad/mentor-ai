f80a0ce44c07050a8c5211451a05ccbd9ce0eed5
Javed Nissar <contact@javednissar.ca>
2024-03-24 18:11:42 -0400

    Added dialogue tree utility for server.

---
+++ server/utils/dialogue-tree.ts
@@ -0,0 +1,50 @@
+import { DialogueNode } from "../types/dialogue-tree";
+
+export class DialogueTree {
+    constructor(nodes: DialogueNode[], childrenMap: { [id: string]: string[] });
+
+    private nodes: DialogueNode[];
+    private childrenMap: { [id: string]: string[] };
+
+    getOptionsFromNode(nodeId: string): DialogueNode[] {
+        const childrenIds = this.childrenMap[this.currentNode.id] || [];
+        return childrenIds.map((id) =>
+            this.nodes.find((node) => node.id === id),
+        );
+    }
+    async addToConversationFromNode(
+        nodeId: string,
+        conversation: Conversation,
+        llmClient: LLMClient,
+    ): Promise<Conversation | null> {
+        const currentNode = this.nodes.find((node) => node.id === nodeId);
+        if (!currentNode) return null;
+
+        if (!currentNode.content) {
+            return conversation;
+        }
+
+        switch (currentNode.content.type) {
+            case "TEXT":
+                conversation.messages.push({
+                    role: "assistant",
+                    text: currentNode.content,
+                });
+                return conversation;
+            case "LARGE_LANGUAGE_MODEL":
+                const newMessage: Message = {
+                    role: "user",
+                    text: currentNode.userQuestionText,
+                };
+                conversation.messages.push(newMessage);
+                const response =
+                    await llmClient.createChatCompletion(conversation);
+                conversation.messages.append({
+                    role: "assistant",
+                    text: response.message,
+                });
+
+                return conversation;
+        }
+    }
+}
