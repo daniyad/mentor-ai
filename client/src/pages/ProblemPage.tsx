cf95f64396e408707189a169ea210ac429cd6d5e
Javed Nissar <contact@javednissar.ca>
2024-05-17 21:25:35 -0400

    Add new submit endpoint

--- client/src/pages/ProblemPage.tsx
+++ client/src/pages/ProblemPage.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { SetStateAction, useEffect, useRef } from "react";
 import { useState } from "react";
 import ReactCodeMirror from "@uiw/react-codemirror";
@@ -268,3 +269,240 @@ const ProblemPage = ({
 };
 
 export default ProblemPage;
+=======
+import React, { SetStateAction, useEffect, useRef } from "react";
+import { useState } from "react";
+import ReactCodeMirror from "@uiw/react-codemirror";
+import { loadLanguage } from "@uiw/codemirror-extensions-langs";
+import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import Chat from "../components/Chat";
+import { useNavigate, useParams } from "react-router-dom";
+import Editorial from "../components/Editorial";
+import MainHeading from "../components/MainHeading";
+import Submissions from "../components/Submissions";
+import HintDisplay from "../components/HintDisplay";
+import { API_URL } from "../App";
+import Loading from "../components/Loading";
+import { Box, Divider, VStack, Text, Button, Slider, SliderTrack, SliderFilledTrack, SliderThumb, Heading, HStack, Card, CardHeader, CardBody, Avatar, Badge } from '@chakra-ui/react';
+import { DescriptionData, Submission, Hint, HintResponse, Message, Option, ProblemDescriptionData } from '../types/general';
+import { Editor } from "@monaco-editor/react"
+import { Resizable } from 're-resizable';
+import { PythonTester } from "../utils/python";
+
+const ProblemPage = ({ }) => {
+    const [username, setUsername] = useState<string>("");
+    const [initCode, setInitCode] = useState<string>("");
+    const [code, setCode] = useState<string>("");
+    const explanationRef = useRef<HTMLDivElement>(null);
+    const sliderRef = useRef<HTMLDivElement>(null);
+    const [currentLang, setCurrentLang] = useState<string>("javascript");
+    const handleSlider = (event: React.MouseEvent<HTMLDivElement>) => {
+        const mouseX = event.clientX;
+        const newWidth = mouseX - 8;
+        if (explanationRef.current)
+            explanationRef.current.style.width = newWidth + "px";
+    };
+
+    const [isSubmitLoading, setIsSubmitLoading] = useState<boolean>(false);
+
+    const [problemDescriptionData, setProblemDescriptionData] =
+        useState<ProblemDescriptionData>();
+    const [submissionStatus, setSubmissionStatus] = useState<boolean>();
+    const [submissionData, setSubmisionData] = useState<Submission>();
+
+    const navigate = useNavigate();
+
+    const [isSolved, setIsSolved] = useState<boolean>(true);
+
+    const [options, setOptions] = useState<Option[]>([]);
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [currentNodeId, setCurrentNodeId] = useState<string>('root');
+    const [pythonTester, setPythonTester] = useState<PythonTester>();
+
+
+    const { courseId, sectionId, problemId } = useParams();
+
+    const submitCode = async () => {
+        setIsSubmitLoading(true);
+
+        if (!courseId || !sectionId || !problemId) {
+            console.log(`the problem for course: ${courseId}, section: ${sectionId}, problem: ${problemId} not found`);
+            setIsSubmitLoading(false);
+            return;
+        }
+
+        if (!problemDescriptionData?.expected_output){
+            console.log(`There is no expected output for this problem: ${courseId}:${sectionId}:${problemId}`);
+            setIsSubmitLoading(false);
+            return;
+        }
+        const expected_output = problemDescriptionData?.expected_output;
+
+        const problemEvaluationResult = await pythonTester?.assertUsingStdOut(code, expected_output);
+
+        if(problemEvaluationResult) {
+            axios.post(
+                `${API_URL}/api/problem_new/submit-v2?courseId=${courseId}&sectionId=${sectionId}&problemId=${problemId}`,
+                { result: problemEvaluationResult },
+                { withCredentials: true },
+            )
+                .then(({ data }) => {
+                    if (data == "Accepted") {
+                        setIsSolved(true)
+                    }
+                    // mark problem as solved, and allow moving to the other person
+                    console.log("LETS GOOOOO EBAT")
+                    alert("Problem solved!");
+                    setIsSubmitLoading(false);
+                })
+                .catch((err) => {
+                    setIsSubmitLoading(false);
+                });
+        } else {
+            if (problemEvaluationResult === null){
+                alert('Failed to evaluate!')
+            } else{
+                alert(`Wrong output! Expected Output:${expected_output}`);
+            }
+            setIsSubmitLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        const fetchProblemData = async () => {
+            try {
+                const response = await axios.get(`${API_URL}/api/problem_new/problem?courseId=${courseId}&sectionId=${sectionId}&problemId=${problemId}`, { withCredentials: true });
+                setProblemDescriptionData(response.data)
+                setCode(response.data?.code_body.code_template ?? "Start coding here")
+            } catch (error) {
+                console.error('Failed to fetch problem data:', error);
+            }
+        };
+
+        const fetchOptions = async () => {
+            try {
+                const response = await axios.post(`${API_URL}/api/mentor/conversation/next`, {
+                    problemId: "hello-world",
+                    nodeId: "root",
+                    messages: messages,
+                });
+
+                setOptions(response.data.options);
+                setMessages(response.data.messages);
+            } catch (error) {
+                console.error('Failed to fetch options:', error);
+            }
+        };
+
+        const setupPythonTester = async () => {
+            try {
+                setPythonTester(new PythonTester());
+            } catch(error) {
+                console.error('Failed to run Python', error);
+            }
+        }
+
+        setupPythonTester();
+        fetchProblemData();
+        fetchOptions();
+    }, [courseId, sectionId, problemId]);
+
+    const handleOptionClick = async (option: Option) => {
+        try {
+            const response = await axios.post(`${API_URL}/api/mentor/conversation/next`, {
+                problemId: 'hello-world',
+                nodeId: option.id,
+                messages: messages,
+            });
+
+            setOptions(response.data.options);
+            setMessages(response.data.messages);
+            setCurrentNodeId(option.id);
+        } catch (error) {
+            console.error('Failed to fetch next conversation step:', error);
+        }
+    };
+
+    return (
+        <>
+            <MainHeading
+                data={{
+                    username: username,
+                }}
+            />
+
+            <div style={{ display: 'flex', height: '100vh' }}>
+                <Resizable
+                    defaultSize={{
+                        width: '50%',
+                        height: '100%',
+                    }}
+                    style={{
+                        overflow: 'auto',
+                        backgroundColor: '#262626', // Background color
+                    }}
+                    handleStyles={{
+                        right: {
+                            background: '#262626',
+                        },
+                    }}
+                >
+                    <Box bg="#333333" pl={3}>
+                        <Text mb={2} ml="" color="white">
+                            <b>Description</b>
+                        </Text>
+                    </Box>
+                    <HStack>
+                        <VStack h="100%" pt={4} pl={4} align={"start"}>
+                            <Heading as="h2" size="lg" color="white" fontWeight="bold">
+                                {`${problemId}. ${problemDescriptionData?.name}`}
+                            </Heading>
+                            <HStack spacing={2} mb={4}>
+                                <Badge colorScheme={problemDescriptionData?.difficulty === "Easy" ? "green" : problemDescriptionData?.difficulty === "Medium" ? "orange" : "red"}>
+                                    {problemDescriptionData?.difficulty}
+                                </Badge>
+                                {problemDescriptionData?.isSolved && <Badge colorScheme="blue">Solved</Badge>}
+                            </HStack>
+                            {
+                                messages.map((message, index) => (
+                                    <HStack key={index} spacing={3}>
+                                        <Avatar name={message.role} size="sm" />
+                                        <Text color="white" maxW='md'>
+                                            {message.text}
+                                        </Text>
+                                    </HStack>
+                                ))
+                            }
+                            {options.map((option, index) => (
+                                <Button key={index} onClick={() => handleOptionClick(option)}>{option.userQuestionText}</Button>
+                            ))}
+                        </VStack>
+                    </HStack>
+                </Resizable>
+                <div style={{ flex: 1 }}>
+                    <Box w="100%" h="90%">
+                        <Editor
+                            height="100%"
+                            language="python"
+                            theme="vs-dark"
+                            value={code}
+                            onChange={(newValue, e) => {
+                                setCode(newValue!!);
+                            }}
+                            options={{ automaticLayout: true }}
+                        />
+                    </Box>
+                    <Box w="100%" h="10%" p={2} bg="#262626" display="flex" justifyContent="flex-end">
+                        <Button colorScheme="blue" size="sm" borderRadius="md" onClick={submitCode} isLoading={isSubmitLoading} loadingText="Submitting">
+                            Submit
+                        </Button>
+                    </Box>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ProblemPage;
+>>>>>>> Stashed changes
---
+++ server/routes/problem/problem-new.ts
@@ -0,0 +1,254 @@
+import express from "express";
+import axios from "axios"
+import { CourseModel } from "../../models/problem-model";
+import authFilter from "../../middlewares/auth-filter"
+import UsersModel from "../../models/user-model";
+
+
+const problem_new = express.Router();
+const judgeApiKey = process.env.JUDGE_API_KEY ?? ""
+
+
+problem_new.get("/course", authFilter, async (req, res) => {
+    try {
+        const user = req.user as User; // User information from session
+        const courseId = parseInt(req.query.courseId as string, 10); // Get courseId from query param
+
+        const course = await CourseModel.findOne({ id: courseId }).populate('sections.problems');
+        if (!course) {
+            res.status(400).send("The requested course doesn't exist")
+            return;
+        }
+        // TODO(Din): Update data model to contain course metadata
+        const courseMetadata = {
+            level: "Beginner",
+            timeToComplete: 25,
+            prerequisites: []
+        }
+
+        // Construct the response object
+        const response = {
+            id: course.id,
+            title: course.title,
+            short_description: course.short_description,
+            description: course.description,
+            skills: course.skills,
+            metadata: courseMetadata,
+            sections: course.sections.map(section => {
+                // Extract section properties
+                const { id, title, short_description, problems } = section;
+
+                // Construct problems info
+                const problemsInfo = problems.map(problem => ({
+                    id: problem.id,
+                    name: problem.name,
+                    difficulty: problem.difficulty,
+                    isSolved: user.attempts.some(attempt =>
+                        attempt.course_id === courseId &&
+                        attempt.section_id === section.id && 
+                        attempt.problem_id === problem.id && 
+                        attempt.status === "SOLVED"
+                    ),
+                }));
+
+                return {
+                    id,
+                    title,
+                    short_description,
+                    problems: problemsInfo,
+                };
+            }),
+        };
+
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).send("Internal server error");
+    }
+});
+
+problem_new.get("/problem", authFilter, async (req, res) => {
+    try {
+        const courseId = parseInt(req.query.courseId as string, 10); // Get courseId from query param
+        const sectionId = parseInt(req.query.sectionId as string, 10); // Get sectionId from query param
+        const problemId = parseInt(req.query.problemId as string, 10); // Get problemId from query param
+        const user = req.user as User; // User information from session
+
+        const course = await CourseModel.findOne({ id: courseId })
+        if (!course) {
+            res.status(400).send("The requested course doesn't exist")
+        }
+
+        const section = course.sections.find(section => section.id == sectionId)
+        if (!section) {
+            res.status(400).send("The requested section doesn't exist")
+            return;
+        }
+
+        // Fetch the problem based on problemId within the section
+        const problem = section.problems.find(p => p.id == problemId);
+
+        if (!problem) {
+            res.status(400).send("Requested problem does not exist");
+            return;
+        }
+
+        // Check if the problem is solved by the user
+        const isSolved = user.attempts.some(attempt =>
+            attempt.course_id === courseId && 
+            attempt.section_id === sectionId &&
+            attempt.problem_id === problemId &&
+            attempt.status == "SOLVED"
+        );
+
+        const pythonCodeTemplate = problem.code_body.find(template => template.language === 'Python');
+        
+        const response = {
+            id: problem.id,
+            name: problem.name,
+            difficulty: problem.difficulty,
+            description_body: problem.description_body,
+            code_body: pythonCodeTemplate,
+            isSolved: isSolved,
+            expected_output: problem.expected_output,
+        }
+
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).send("Internal server error");
+    }
+});
+
+problem_new.post("/submit-v2", authFilter, async (req, res) => {
+    const courseId = parseInt(req.query.courseId as string, 10); // Get courseId from query param
+    const sectionId = parseInt(req.query.sectionId as string, 10); // Get sectionId from query param
+    const problemId = parseInt(req.query.problemId as string, 10); // Get problemId from query param
+
+    const user = req.user as User;
+    const course = await CourseModel.findOne({ id: courseId })
+    if (!course) {
+        res.status(400).send("The requested course doesn't exist")
+    }
+
+    const section = course.sections.find(section => section.id == sectionId)
+    if (!section) {
+        res.status(400).send("The requested section doesn't exist")
+        return;
+    }
+
+    // Fetch the problem based on problemId within the section
+    const problem = section.problems.find(p => p.id == problemId);
+
+    if (!problem) {
+        res.status(400).send("Requested problem does not exist");
+        return;
+    }
+
+    const dbUser = await UsersModel.findById(user.id); // Fetch the user from the database
+    dbUser.attempts.push({
+        section_id: sectionId,
+        course_id: courseId,
+        problem_id: problemId,
+        status: "SOLVED"
+    });
+    await dbUser.save(); // Save the user
+
+    res.status(200).json({ "status": "SUCCESS"})
+})
+
+problem_new.post("/submit", authFilter, async (req, res) => {
+    const courseId = parseInt(req.query.courseId as string, 10); // Get courseId from query param
+    const sectionId = parseInt(req.query.sectionId as string, 10); // Get sectionId from query param
+    const problemId = parseInt(req.query.problemId as string, 10); // Get problemId from query param
+    const code = req.body.code as string // Get submitted code
+
+    const user = req.user as User; // User information from session
+    const course = await CourseModel.findOne({ id: courseId })
+    if (!course) {
+        res.status(400).send("The requested course doesn't exist")
+    }
+
+    const section = course.sections.find(section => section.id == sectionId)
+    if (!section) {
+        res.status(400).send("The requested section doesn't exist")
+        return;
+    }
+
+    // Fetch the problem based on problemId within the section
+    const problem = section.problems.find(p => p.id == problemId);
+
+    if (!problem) {
+        res.status(400).send("Requested problem does not exist");
+        return;
+    }
+
+    const expectedOutput = problem.expected_output
+    const submissionUrl = "https://judge0-ce.p.rapidapi.com/submissions?base64_encoded=true&fields=*"
+    const pythonLanguageId = 71
+
+    const submissionHeaders = {
+        "Accept": "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+        "X-RapidAPI-Key": judgeApiKey,
+        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+    }
+
+    const optionsForSubmittingCode = {
+        method: "POST",
+        url: submissionUrl,
+        headers: submissionHeaders,
+        data: {
+            "language_id": pythonLanguageId,
+            "source_code": stringToBase64(code),
+            "expected_output": stringToBase64(expectedOutput)
+        }
+    }
+
+    const submissionResponse = await axios(optionsForSubmittingCode);
+    if (!submissionResponse || submissionResponse.status != 201) {
+        res.status(500).send("There was an error in our servers, please try again later")
+        return
+    }
+
+    //waiting for the code to be complete
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    const token = submissionResponse.data.token
+    const submissionStatusUrl =
+        "https://judge0-ce.p.rapidapi.com/submissions/" + token + "?base64_encoded=true&fields=*"
+    const submissionStatusHeaders = {
+        "X-RapidAPI-Key": judgeApiKey,
+        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+    }
+
+    const optionsForRetrievingResult = {
+        method: "GET",
+        url: submissionStatusUrl,
+        headers: submissionStatusHeaders,
+    }
+
+    const codeSubmissionResponse = await axios(optionsForRetrievingResult)
+    if (!codeSubmissionResponse || codeSubmissionResponse.status != 200) {
+        res.status(500).send("There was an error in our servers, please try again later")
+        return
+    }
+    const submissionStatus = codeSubmissionResponse.data.status.description
+    if (submissionStatus == "Accepted") {
+        console.log("HELLOOOOO SOLVED")
+        const dbUser = await UsersModel.findById(user.id); // Fetch the user from the database
+        dbUser.attempts.push({
+            section_id: sectionId,
+            course_id: courseId,
+            problem_id: problemId,
+            status: "SOLVED"
+        });
+        await dbUser.save(); // Save the user
+    }
+    
+    res.status(200).json(codeSubmissionResponse.data.status.description)
+})
+
+function stringToBase64(str: string): string {
+    return Buffer.from(str).toString('base64');
+}
+
+export default problem_new;
